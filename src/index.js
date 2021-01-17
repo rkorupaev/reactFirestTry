4bb7f9497fdb3366186e8a5182799bba8debdf26
Roman Korupaev <rkorupaev@gmail.com>
2021-01-17 18:24:56 +0300

    store created

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import state from './redux/state';
-import {entireRerender} from "./render";
+import store from './redux/state';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-entireRerender(state);
+let entireRerender = (state) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App state={state} addTextElement={store.addTextElement.bind(store)} changeTextarea={store.changeTextarea.bind(store)}/>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+};
+
+entireRerender(store.getState());
+store.subscribe(entireRerender);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,89 +1,93 @@
-import {entireRerender} from "../render";
-
-let state = {
-    dialogsPage: {
-        messagesArray: [
-            {
-                id: 1,
-                item_text: "Привет!"
-            }, {
-                id: 2,
-                item_text: "Как дела?"
-            }, {
-                id: 3,
-                item_text: "Уже приехали?!"
-            }, {
-                id: 4,
-                item_text: "Новое сообщение"
-            }
-        ],
-        dialogsArray: [
-            {
-                item_name: "Dima",
-                item_id: "1"
-            }, {
-                item_name: "Olya",
-                item_id: "2"
-            }, {
-                item_name: "Vasya",
-                item_id: "3"
-            }, {
-                item_name: "John",
-                item_id: "4"
-            }, {
-                item_name: "Kate",
-                item_id: "5"
-            }, {
-                item_name: "Igor",
-                item_id: "6"
-            }
-        ]
+let store = {
+    _state: {
+        dialogsPage: {
+            messagesArray: [
+                {
+                    id: 1,
+                    item_text: "Привет!"
+                }, {
+                    id: 2,
+                    item_text: "Как дела?"
+                }, {
+                    id: 3,
+                    item_text: "Уже приехали?!"
+                }, {
+                    id: 4,
+                    item_text: "Новое сообщение"
+                }
+            ],
+            dialogsArray: [
+                {
+                    item_name: "Dima",
+                    item_id: "1"
+                }, {
+                    item_name: "Olya",
+                    item_id: "2"
+                }, {
+                    item_name: "Vasya",
+                    item_id: "3"
+                }, {
+                    item_name: "John",
+                    item_id: "4"
+                }, {
+                    item_name: "Kate",
+                    item_id: "5"
+                }, {
+                    item_name: "Igor",
+                    item_id: "6"
+                }
+            ]
+        },
+        profilePage: {
+            postsArray: [
+                {
+                    id: 3,
+                    post_text: "Not rly, just kidding)",
+                    posts_likes: "15"
+                },
+                {
+                    id: 2,
+                    post_text: "I am so cool!",
+                    posts_likes: "25"
+                },
+                {
+                    id: 1,
+                    post_text: "First post! Hello world!",
+                    posts_likes: "135"
+                }
+            ],
+            newText: ""
+        },
+        sidebar: {
+            sidebarFriends: [
+                {name: "Дима Иванов"},
+                {name: "Оля Васина"},
+                {name: "Kate Pressly"}
+            ]
+        }
     },
-    profilePage: {
-        postsArray: [
-            {
-                id: 3,
-                post_text: "Not rly, just kidding)",
-                posts_likes: "15"
-            },
-            {
-                id: 2,
-                post_text: "I am so cool!",
-                posts_likes: "25"
-            },
-            {
-                id: 1,
-                post_text: "First post! Hello world!",
-                posts_likes: "135"
-            }
-        ],
-        newText: ""
+    getState() {
+        return this._state;
     },
-    sidebar: {
-        sidebarFriends: [
-            {name: "Дима Иванов"},
-            {name: "Оля Васина"},
-            {name: "Kate Pressly"}
-        ]
+    _callRender() {
+    },
+    subscribe(observe) {
+        this._callRender = observe;
+    },
+    addTextElement() {
+        let newElement = {
+            id: this._state.profilePage.postsArray.length + 1,
+            post_text: this._state.profilePage.newText,
+            posts_likes: 0
+        };
+        this._state.profilePage.postsArray.unshift(newElement);
+        this._state.profilePage.newText = '';
+        this._callRender(this._state);
+    },
+    changeTextarea(change) {
+        this._state.profilePage.newText = change;
+        this._callRender(this._state);
     }
 };
 
-export let addTextElement = () => {
-    let newElement = {
-        id: state.profilePage.postsArray.length + 1,
-        post_text: state.profilePage.newText,
-        posts_likes: 0
-    };
-    state.profilePage.postsArray.unshift(newElement);
-    state.profilePage.newText = '';
-    console.log(state.profilePage.newText);
-    entireRerender(state);
-};
-
-export let changeTextarea = (change) => {
-    state.profilePage.newText = change;
-    entireRerender(state);
-};
-
-
-export default state;
+export default store;
