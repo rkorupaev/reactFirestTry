d3864c96784a63d7ba9c0821f1290985bdc03c4e
Roman Korupaev <rkorupaev@gmail.com>
2021-03-02 22:26:40 +0300

    representative comp refactor

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -1,69 +1,72 @@
 import React from "react";
 import style from "./Friends.module.css";
-import * as axios from "axios";
+import userDefaultIcon from "./../../img/user.png";
 
-class FriendsList extends React.Component {
+let FriendsList = (props) => {
 
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${this.props.currentPage}`).then(response => {
-            this.props.setFriends(response.data.items)
-        });
-    };
+    // componentDidMount() {
+    //     axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${this.props.currentPage}`).then(response => {
+    //         this.props.setFriends(response.data.items)
+    //         this.props.setTotalPages(response.data.totalCount)
+    //     });
+    // };
+    //
+    // onPageButtonClickHandler = (pageNumber) => {
+    //     this.props.onPageButtonClick(pageNumber);
+    //     axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${pageNumber}`).then(response => {
+    //         this.props.setFriends(response.data.items)
+    //     });
+    // };
 
-    onPageButtonClickHandler = (pageNumber) => {
-        this.props.onPageButtonClick(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${pageNumber}`).then(response => {
-            this.props.setFriends(response.data.items)
-        });
-    };
+    let totalPageCount = Math.ceil(props.totalUsersCount / props.pageSize);
 
-
-    render() {
-        let totalPageCount = Math.ceil(this.props.totalCount / this.props.pageSize);
-
-        let pageButtonsArray = [];
-        for (let i = 0; i < totalPageCount; i++) {
-            pageButtonsArray.push(i + 1);
-        }
-
-        return (
-            <div>
-                <ul className={style.friendlist__pagination}>
-                    {pageButtonsArray.map(page => <li onClick={(evt) => {
-                        this.onPageButtonClickHandler(page);
-                    }}
-                                                      className={[style.pagination__item, this.props.currentPage === page && style.activepage].join(' ')}>{page}</li>)}
-                </ul>
-                <ul className={style.friendlist}>
-                    {
-                        this.props.friendsArray.map(friend =>
-                            <li className={style.friendlist__item}>
-                                <div className={style.item__infoblock}>
-                                    <div className={style.item__avablock}>
-                                        <img className={style.item__image}
-                                             src={friend.photos.large}
-                                             alt={friend.name}/>
-                                        {friend.followed ?
-                                            <button onClick={() => this.props.onFollowButtonCLick(friend.id)}
-                                                    className={style.item__button}>Follow</button> :
-                                            <button onClick={() => this.props.onFollowButtonCLick(friend.id)}
-                                                    className={style.item__button}>Unfollow</button>
-                                        }
-                                    </div>
-                                    <div className={style.item__bio}>
-                                        <h2>{friend.name}</h2>
-                                        <p><span>friend.age</span>года</p>
-                                        <p>friend.location</p>
-                                    </div>
-                                </div>
-                                <div className={style.item__status}>
-                                    <p>friend.item_text</p>
-                                </div>
-                            </li>
-                        )}
-                </ul>
-            </div>)
+    let pageButtonsArray = [];
+    for (let i = 0; i < totalPageCount; i++) {
+        pageButtonsArray.push(i + 1);
     }
+
+    let pageButtonsArrayVisible = [];
+    for (let i = 0; i < 5; i++) {
+        pageButtonsArrayVisible.push(pageButtonsArray[i]);
+    }
+
+    return (
+        <div>
+            <ul className={style.friendlist__pagination}>
+                {pageButtonsArrayVisible.map(page => <li onClick={(evt) => {
+                    props.onPageButtonClickHandler(page);
+                }}
+                                                         className={[style.pagination__item, props.currentPage === page && style.activepage].join(' ')}>{page}</li>)}
+            </ul>
+            <ul className={style.friendlist}>
+                {
+                    props.friendsArray.map(friend =>
+                        <li className={style.friendlist__item}>
+                            <div className={style.item__infoblock}>
+                                <div className={style.item__avablock}>
+                                    <img className={style.item__image}
+                                         src={friend.photos.large != null ? friend.photos.large : userDefaultIcon}
+                                         alt={friend.name}/>
+                                    {friend.followed ?
+                                        <button onClick={() => props.onFollowButtonCLick(friend.id)}
+                                                className={style.item__button}>Follow</button> :
+                                        <button onClick={() => props.onFollowButtonCLick(friend.id)}
+                                                className={style.item__button}>Unfollow</button>
+                                    }
+                                </div>
+                                <div className={style.item__bio}>
+                                    <h2>{friend.name}</h2>
+                                    <p><span>friend.age</span>года</p>
+                                    <p>friend.location</p>
+                                </div>
+                            </div>
+                            <div className={style.item__status}>
+                                <p>friend.item_text</p>
+                            </div>
+                        </li>
+                    )}
+            </ul>
+        </div>)
 };
 
 export default FriendsList;
--- src/components/Friends/Friends_container.jsx
+++ src/components/Friends/Friends_container.jsx
@@ -1,12 +1,38 @@
 import React from "react";
 import {connect} from "react-redux";
 import FriendsList from "./Friends";
-import {changeStatusAC, setCurrentPageAC, setFriendsAC} from "../../redux/friendsPageReducer";
+import {changeStatusAC, setCurrentPageAC, setFriendsAC, setTotalPagesAC} from "../../redux/friendsPageReducer";
+import * as axios from "axios";
+
+
+class FriendsListApi extends React.Component {
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${this.props.currentPage}`).then(response => {
+            this.props.setFriends(response.data.items)
+            this.props.setTotalPages(response.data.totalCount)
+        });
+    };
+
+    onPageButtonClickHandler = (pageNumber) => {
+        this.props.onPageButtonClick(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${pageNumber}`).then(response => {
+            this.props.setFriends(response.data.items)
+        });
+    };
+
+    render() {
+        return <FriendsList totalUsersCount={this.props.totalUsersCount} pageSize={this.props.pageSize}
+                            onPageButtonClickHandler={this.onPageButtonClickHandler}
+                            currentPage={this.props.currentPage}
+                            friendsArray={this.props.friendsArray}
+                            onFollowButtonCLick={this.props.onFollowButtonCLick}/>;
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
         friendsArray: state.friendsPage.friendsArray,
-        totalCount: state.friendsPage.totalCount,
+        totalUsersCount: state.friendsPage.totalUsersCount,
         pageSize: state.friendsPage.pageSize,
         currentPage: state.friendsPage.currentPage
     }
@@ -22,10 +48,13 @@ let mapDispatchToProps = (dispatch) => {
         },
         onPageButtonClick: (pageNumber) => {
             dispatch(setCurrentPageAC(pageNumber));
+        },
+        setTotalPages: (totalUsers) => {
+            dispatch(setTotalPagesAC(totalUsers));
         }
     }
 };
 
-const FriendsListContainer = connect(mapStateToProps, mapDispatchToProps)(FriendsList);
+const FriendsListContainer = connect(mapStateToProps, mapDispatchToProps)(FriendsListApi);
 
 export default FriendsListContainer;
