6f19c61fbcd00c7b5ceb8311f8861e6b97f92869
Roman Korupaev <rkorupaev@gmail.com>
2021-04-20 21:31:54 +0300

    final form implemented

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,23 +2,24 @@ import React from "react";
 import style from "./Login.module.css";
 import {Form, Field} from "react-final-form";
 
+const onSubmit = (values) => {
+    console.log(values);
+}
 
-const LoginForm = () => {
-    <Form onSubmit={onSubmit}>
-        {props => (
-            <form onSubmit={props.handleSubmit}>
-
+const LoginForm = () => (
+    <Form
+        onSubmit={onSubmit}
+        render={({handleSubmit, values}) => (
+            <form className={style.loginForm} onSubmit={handleSubmit}>
                 <label>Login: <Field name={"login"} placeholder={"Login"} component={"input"} type={"text"}/></label>
                 <label>Password: <Field name={"password"} placeholder={"Password"} component={"input"}
                                         type={"text"}/></label>
                 <label>Remember me: <Field name={"remembered"} type={"checkbox"} component={"input"}/></label>
                 <button className={style.loginForm__button} type={"submit"}>Register!</button>
-
-                <button type="submit">Submit</button>
             </form>
         )}
-    </Form>
-}
+    />
+)
 
 const Login = () => {
     return (
